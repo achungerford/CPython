b70517c8dc6dbefda01ddb15de0a82f06a5ffc8a
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-06-20 00:06:01 -0700

    initial commit of C files

---
+++ 00_basic.c
@@ -0,0 +1,21 @@
+/*
+    Simple C program
+
+    gcc compile:
+        open cmd line
+        gcc -o fileNameOutput.exe sourceCodeFileName.c
+
+    gcc run:
+        fileNameOutput  (to run)
+*/
+
+#include <stdio.h>  // preprocessor/header files
+
+int main(void)  // start program
+{
+    
+    printf("Start program here.\n");
+
+    // system("pause");
+    return 0;  // end program
+}
