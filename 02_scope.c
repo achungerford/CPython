76a703a8f37b57f459888c52ec9cb956c42f40ee
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-06-20 09:11:36 -0700

    show scope of function

---
+++ 02_scope.c
@@ -0,0 +1,60 @@
+/*
+    Demonstrating Python's PEP 7 formatting for C programs.
+*/
+
+    // C format choices
+        // define functions before you call them OR
+        // declare the functions, then call them, define below the call
+
+/*
+     for anything before Python 3.6
+     all declarations must be at the top of a block
+     (not necessarily at the top of function)
+*/
+
+
+
+/*
+===============================================================================
+Example 2: Function Declarations before the call & definition
+
+===============================================================================
+*/
+
+// preprocessor/header files
+#include <stdio.h>
+#include <strings.h>
+
+/*
+    function declarations rquire a semicolon;
+    only needs variable types (int, int), not names
+    function type is void bc no return statement;
+*/
+
+// function declaration
+void superMan(void);
+
+int main(void)  // start program
+{
+    int superman = 20;
+    printf("Start program here.\n");    // first line, new line symbol
+    superMan();                        // control jumps to superMan below
+    printf("%d\n", superman);
+    printf("\nEnd program here.\n");
+
+    // system("pause");
+    return 0;           // end program
+}
+
+
+/*  FUNCTION DEFINIION BELOW
+    function name = superMan
+    function type = void
+    function declares 3 variables = super, man, superman
+    function has no arguments (void);
+*/
+void superMan(void)
+{
+    int super = 10, man = 5, superman = super + man;
+    printf("%d\n", superman);
+}
